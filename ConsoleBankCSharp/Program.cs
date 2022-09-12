03870572d8971fdc6ff9e6958ed43d63afb541bd
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-12 17:23:33 +0200

    added static void main

--- ConsoleBankCSharp/Program.cs
+++ ConsoleBankCSharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleBankCSharp.Models;
-Console.WriteLine("Hello, World!");
-Client client = new Client( 1 , "joe mama");
+
+static void Main()
+{
+    
+}
