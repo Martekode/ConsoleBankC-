8f978d2c910f51b32f21cd0ee8ba2385ceaa527f
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-12 17:24:30 +0200

    fouth option singleton implemented with makeclient and choosebankaccount

---
+++ ConsoleBankCSharp/Models/App.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleBankCSharp.Models;
+
+public sealed class App
+{
+    private static readonly App instance = new App();
+
+    static App()
+    {
+    }
+
+    private App()
+    {
+    }
+
+    public static App Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public Client MakeClient(int id , string name)
+    {
+        Client client = new Client(id, name);
+        return client;
+    }
+
+    public BankAccount ChooseBankAccount(Client client, int InitDeposit , int InitType )
+    {
+        BankAccount bankAccount = new BankAccount(client , InitDeposit, InitType );
+        return bankAccount;
+    }
+}
