007fd92d48006a74964669e62b079df71b899351
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-13 02:56:49 +0200

    methods nade for the singleton. events

--- ConsoleBankCSharp/Models/App.cs
+++ ConsoleBankCSharp/Models/App.cs
@@ -20,15 +20,48 @@ public sealed class App
         }
     }
 
-    public Client MakeClient(int id , string name)
+    private static string EnterName()
     {
-        Client client = new Client(id, name);
+        Console.WriteLine("Please give enter your name:");
+        string response = Console.ReadLine();
+        if (!String.IsNullOrEmpty(response))
+        {
+            return response;
+        }
+        else
+        {
+            Console.WriteLine($"Please fill in your name!!");
+            response = EnterName();
+        }
+        return response;
+    }
+    public static Client MakeClient()
+    {
+        Random rnd = new Random();
+        int id = rnd.Next();
+        string responseName = EnterName();
+        Client client = new Client(id, responseName);
         return client;
     }
 
-    public BankAccount ChooseBankAccount(Client client, int InitDeposit , int InitType )
+    public static BankAccount ChooseBankAccount(Client client)
     {
+        Console.WriteLine($"Hello {client.name}, what type of account do you want to make?");
+        Console.WriteLine("There are 3 kind of accounts;");
+        Console.WriteLine("1: Checking Account");
+        Console.WriteLine("This account is regular with non-negative balance and deposit/withdraw rights");
+        Console.WriteLine("2: Saving Account");
+        Console.WriteLine("This account only has deposit rights. To withdraw funds, contact ous via mail or via cell: 0478643605");
+        Console.WriteLine("3: Credit Account");
+        Console.WriteLine("This account has all the checking account rights,");
+        Console.WriteLine(" but also can go up to 2500 euro's negative");
+        Console.WriteLine("------------------------------------------------------");
+        Console.WriteLine("Make your choice! [ 1 , 2 , 3]");
+        int InitType = Int32.Parse(Console.ReadLine() ?? "1");
+        Console.WriteLine("Now enter a minimum deposit of 10€");
+        int InitDeposit = Int32.Parse(Console.ReadLine() ?? "10");
         BankAccount bankAccount = new BankAccount(client , InitDeposit, InitType );
+        Console.WriteLine($"{client.name}, your bank account was successfully set up");
         return bankAccount;
     }
 }
