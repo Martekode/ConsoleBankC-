a09fac2c67f9889cee037d376151df5fbe848481
Martekode <BrianDesmedt9000@hotmail.com>
2022-09-13 02:58:22 +0200

    added main method loop with App events

--- ConsoleBankCSharp/Program.cs
+++ ConsoleBankCSharp/Program.cs
@@ -3,5 +3,44 @@ using ConsoleBankCSharp.Models;
 
 static void Main()
 {
-    
+    Client client = null;
+    BankAccount bankAccount;
+    Console.WriteLine("Hello dear (Potential) Customer!");
+    Console.WriteLine("Would you like to register as a new Client? [yes / no]");
+    string makeClientYesNo = Console.ReadLine();
+    if (string.IsNullOrEmpty(makeClientYesNo))
+    {
+        Console.WriteLine($"{makeClientYesNo} was not a valid input! Please try again?");
+        Console.WriteLine("Would you like to register as a new Client? [yes / no]");
+        makeClientYesNo = Console.ReadLine();
+    }
+
+    switch (makeClientYesNo)
+    {
+        case "yes" or "y" or "Yes" or "1":
+            Console.WriteLine("client is being made!");
+            client = App.MakeClient();
+            break;
+        case "no":
+            Console.WriteLine("Sorry to here that, have a nice day!");
+            break;
+    }
+
+    if (client is not null)
+    {
+        Console.WriteLine($"Hello {client.name}.");
+        Console.WriteLine("Do you want to make a bank account? [ yes / no ]");
+        string chooseBankAccountYesNo = Console.ReadLine() ?? throw new InvalidOperationException();
+        switch (chooseBankAccountYesNo)
+        {
+            case "yes" or "y" or "Yes" or "1":
+                bankAccount = App.ChooseBankAccount(client);
+                break;
+            case "no" or "n" or "No" or "NO" or "2":
+                Console.WriteLine("Sorry to hear that.");
+                Console.WriteLine("No account was made!");
+                break;
+        }
+    }
 }
+Main();
